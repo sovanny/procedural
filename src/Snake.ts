499345a8ffaa3e665aa54b2f5bc3f60992977c67
Simon Hedlund <simhe813@student.liu.se>
2019-01-09 22:07:57 +0100

    speed is a private variable

--- src/Snake.ts
+++ src/Snake.ts
@@ -3,10 +3,12 @@ export class Snake {
 
   private _position: THREE.Vector2
   private _direction: THREE.Vector2
+  private _speed: number
  
   constructor() {
     this._position = new THREE.Vector2(0.5, 0.5)
     this._direction = new THREE.Vector2(0.5, 0.5)
+    this._speed = 0.007
   }
 
   public setup = () => {
@@ -14,7 +16,7 @@ export class Snake {
   }
 
   public move = (time: number) => {
-    const direction = this.direction.clone().multiplyScalar(0.007)
+    const direction = this.direction.clone().multiplyScalar(this._speed)
     this._position.add(direction)
     if (this.position.x < 0) { this.position.x = 1 }
     if (this.position.y < 0) { this.position.y = 1 }
