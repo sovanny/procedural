38b923e9483d8b231cdd015b9af99bf970e955f3
Sovanny <shuy@Sovannys-MBP.lan>
2019-01-10 17:20:40 +0100

    removed if-statement in shader; stegu doesn't like them

--- src/shaders/fragmentShader.ts
+++ src/shaders/fragmentShader.ts
@@ -1,4 +1,7 @@
 export const fragmentShader = `
+  #ifdef GL_ES
+  precision highp float;
+  #endif
   uniform float time;
   uniform vec2 resolution;
   uniform vec2 snakePosition;
@@ -10,13 +13,11 @@ export const fragmentShader = `
     float x = gl_FragCoord.x / resolution.x;
     float y = gl_FragCoord.y / resolution.y;
     
-    vec3 color = vec3(1.0, 0.0, 0.0);
-    if ( length(snakePosition - vec2(x, y)) < snakeRadius ) {
-      color = vec3(0.0, 0.0, 0.0);
-    }else if ( length(candyPosition - vec2(x, y)) < candyRadius ) {
-      color = vec3(0.0, 1.0, 1.0);
-    }
+    vec3 snakeColor = vec3(1.0, 0.0, 0.0) * step(length(snakePosition - vec2(x, y)), snakeRadius);
+    vec3 candyColor = vec3(0.0, 1.0, 1.0) * step(length(candyPosition - vec2(x, y)), candyRadius);
+    vec3 notBGColors = snakeColor + candyColor;
     
-    gl_FragColor = vec4(color, 1.0);
+    vec3 backgroundColor = vec3(0.5, 0.5, 0.5) * step(length(notBGColors), 0.0);
+    gl_FragColor = vec4(backgroundColor + notBGColors, 1.0);
   }
 `
