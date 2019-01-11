f6457ccaf49cd434f8b5120ee624b38184a88861
Sovanny <shuy@c-10-253-245-225.clients.eduroam.liu.se>
2019-01-11 15:15:03 +0100

    candy has color  and pattern

--- src/Candy.ts
+++ src/Candy.ts
@@ -4,19 +4,30 @@ export class Candy {
 
   private _position: THREE.Vector2
   private _radius: number
+  private _color: THREE.Vector3
+  private _colorSchemes: THREE.Vector3[]
+  private _nColors: number
  
   constructor() {
     this._position = new THREE.Vector2(0.5, 0.5)
     this._radius = 0.02
+    this._color = new THREE.Vector3(0.5, 0.5, 0.5)
+    this._nColors = 2
+    this._colorSchemes = new Array(this._nColors)
   }
 
   public setup = () => {
     this.setRandPosition(0+this.radius, new THREE.Vector2(0.4, 0.4), new THREE.Vector2(0.6, 0.6), 1-this.radius)
+    this.setColors()
+    let newColor = (this._colorSchemes[THREE.Math.randInt(0,this._nColors-1)])
+    this.color.set(newColor.x, newColor.y, newColor.z)
   }
 
   public spawn = (snakePosition: THREE.Vector2) => {
     let excludeLower = snakePosition.subScalar(0.2);
     let excludeUpper = snakePosition.addScalar(0.2);
+    let newColor = (this._colorSchemes[THREE.Math.randInt(0,this._nColors-1)])
+    this.color.set(newColor.x, newColor.y, newColor.z)
     this.setRandPosition(0+this.radius, excludeLower, excludeUpper, 1-this.radius);
   }
 
@@ -32,10 +43,20 @@ export class Candy {
     this._position.set(x, y);
   }
 
+  private setColors = () => {
+    // watermelon color
+    this._colorSchemes[0] = new THREE.Vector3(0.3, 0.43, 0.12)
+    // polka color
+    this._colorSchemes[1] = new THREE.Vector3(1.0, 0.0, 0.0)
+  }
+
   //set position(p: THREE.Vector2) { this._position = p } 
   set radius(r: number){ this._radius = r}
+  set color(c: THREE.Vector3){ this._color = c }
   
   get position() { return this._position } 
   get radius() {return this._radius}
+  get color() {return this._color}
+  get colors() {return this._colorSchemes}
 }
 
--- src/shaders/fragmentShader.ts
+++ src/shaders/fragmentShader.ts
@@ -8,16 +8,43 @@ export const fragmentShader = `
   uniform float snakeRadius;
   uniform vec2 candyPosition;
   uniform float candyRadius;
+  uniform vec3 candyColorScheme;
   uniform int score;
 
   void main()	{
     float x = gl_FragCoord.x / resolution.x;
     float y = gl_FragCoord.y / resolution.y;
+
+    /**** CANDY ****/
+    float candyR, candyG, candyB;
     
+    float cX = (x - candyPosition.x) / candyRadius;
+    float cY = (y - candyPosition.y) / candyRadius;
+
+    float divides = 8.0;
+    float minorNoise  = float(score);
+
+    float atanAngle = atan(cX, cY) + minorNoise;
+    float intensity = floor(sin(atanAngle*divides));
+
+    candyR = candyColorScheme.x + 0.16*intensity;
+    candyG = candyColorScheme.y + 0.16*intensity;
+    candyB = candyColorScheme.z + 0.06*intensity;
+
+    //vec3 candyColor = vec3(0.3, 1.0, 1.0) * step(length(candyPosition - vec2(x, y)), candyRadius);
+    vec3 candyColor = vec3(candyR, candyG, candyB)* step(length(candyPosition - vec2(x, y)), candyRadius);;
+
+
+
+    /**** SNAKE ****/
     vec3 snakeColor = vec3(1.0, 0.3, 0.3) * step(length(snakePosition - vec2(x, y)), snakeRadius);
-    vec3 candyColor = vec3(0.3, 1.0, 1.0) * step(length(candyPosition - vec2(x, y)), candyRadius);
-    vec3 notBGColors = snakeColor + candyColor;
+
+
+    /**** BG    ****/
     vec3 backgroundColor = vec3(0.75, 0.7, 0.5);
+
+    
+    vec3 notBGColors = snakeColor + candyColor;
     backgroundColor = backgroundColor * step(length(notBGColors), 0.0);
     gl_FragColor = vec4(backgroundColor + notBGColors, 1.0);
   }
