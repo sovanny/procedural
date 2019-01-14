907072bfc99606c3f1af5439b0eb47cbd2379bde
Sovanny <shuy@c-10-253-245-225.clients.eduroam.liu.se>
2019-01-14 15:19:19 +0100

    candy segments bulging outwards

--- src/shaders/candyShader.ts
+++ src/shaders/candyShader.ts
@@ -4,27 +4,29 @@ export const candyShader = `
 
     float cX = (x - candyPosition.x) / candyRadius;
     float cY = (y - candyPosition.y) / candyRadius;
+    float candyDist = length(candyPosition - vec2(x, y));
 
     float divides = 8.0;
     float minorNoise  = sin(pow(float(score), 2.0) + length(vec2(cX, cY)));
 
     float atanAngle = atan(cX , cY );
-    float intensity = abs(floor(sin((atanAngle+ minorNoise)*divides)));
+    float intensity = sin((atanAngle+ minorNoise)*divides);
 
-    candyR = candyColorBase.x + candyColorAccent.x*intensity;
-    candyG = candyColorBase.y + candyColorAccent.y*intensity;
-    candyB = candyColorBase.z + candyColorAccent.z*intensity;
+    candyR = candyColorBase.x + candyColorAccent.x*abs(floor(intensity));
+    candyG = candyColorBase.y + candyColorAccent.y*abs(floor(intensity));
+    candyB = candyColorBase.z + candyColorAccent.z*abs(floor(intensity));
 
-    float candyDist = length(candyPosition - vec2(x, y));
+    float bulgeFactor = abs(intensity*candyRadius*0.1);
 
     //vec3 candyColor = vec3(0.3, 1.0, 1.0) * step(length(candyPosition - vec2(x, y)), candyRadius);
-    vec3 candyColor = vec3(candyR, candyG, candyB)* step(candyDist, candyRadius);
+    vec3 candyColor = vec3(candyR, candyG, candyB)* step(candyDist, candyRadius + bulgeFactor);
 
     float normDist = candyDist/candyRadius;
 
-    if(candyDist < candyRadius){
+    if(candyDist < candyRadius){ //ta bort if-sats
         texNorm = normalize(vec3(normDist, normDist, normDist));
     }
+
 
 
 `
