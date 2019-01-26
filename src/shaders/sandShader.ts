7571e7c5576af4bbc53604f63471cfd3ba40e82e
Sovanny <shuy@MJ1-2015-0341L.localdomain>
2019-01-26 20:52:36 +0100

    basic sand

--- src/shaders/sandShader.ts
+++ src/shaders/sandShader.ts
@@ -4,13 +4,13 @@ export const sandShader = `
     vec3 sandBaseColor = vec3(0.75, 0.7, 0.5);
 
     sandBaseColor = sandBaseColor*0.5 + (sandBaseColor*max(sandBaseNoise, 0.5));
-    float glimmerNoise = snoise(vec3(x*8.0, y*4.0, time*0.02));
-    glimmerNoise = glimmerNoise*0.5 + snoise(vec3(x*16.0, y*8.0 + time*0.02, 0.0));
-    glimmerNoise = glimmerNoise*0.2 + snoise(vec3(x*32.0, y*16.0, 0.0));
+    float glimmerNoise = sandBaseNoise + snoise(vec3(x*8.0, y*4.0, 20.0));
+    glimmerNoise = glimmerNoise*0.3 + snoise(vec3(x*16.0, y*8.0,200.0));
+    glimmerNoise = glimmerNoise*0.15 + snoise(vec3(x*32.0, y*16.0, 500.0));
 
     vec3 glimmer = vec3(0.0);
-    if(glimmerNoise > 0.95){
-        glimmer = vec3(0.0, 0.8, 0.8);
+    if( mod(ceil(glimmerNoise*1000.0), 3.14) == 0.0 ){
+        glimmer = vec3(0.5, 0.8, 0.8);
     }
 
     vec3 backgroundColor = sandBaseColor + glimmer;
